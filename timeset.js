aaf36fb3ac269f87567803b4acdd12dc2f4ae535
Jonah Pelfrey <pelfreyjonah@gmail.com>
2019-06-06 15:32:02 -0500

    timer class, mark functionality

--- timeset.js
+++ timeset.js
@@ -1,61 +1,126 @@
+const Timer = require('./lib/timer.js');
+
 function TimeSet() {
 	this.timers = {};
-	this.results = [];
 }
 
-/* Start timer corresponding to name */
-TimeSet.prototype.start = function(name) {
-	let t = process.hrtime();
-	let id = timer_id_for(name, "-start");
+TimeSet.prototype.add = function(name) {
+	let id = timer_id_for(name);
 
 	if (this.timers[id] == undefined) {
-		this.timers[id] = t;
+		let timer = new Timer(name);
+		this.timers[id] = { 
+			name: name,
+			timer: timer,
+			marks: [] 
+		}
 	}
 }
 
-/* End timer corresponding to name */
+TimeSet.prototype.start = function(name) {
+	let id = timer_id_for(name);
+
+	if (this.timers[id] != undefined) {
+		this.timers[id].timer.start();
+	}
+}
+
+TimeSet.prototype.mark = function(name, mark) {
+	
+	let markTimer = new Timer(mark);
+	markTimer.start();
+	
+	let id = timer_id_for(name);
+
+	if (this.timers[id] != undefined) {
+
+		if (this.timers[id].marks.length > 0) {
+			let index = this.timers[id].marks.length;
+			this.timers[id].marks[index-1].end();
+		}
+
+		this.timers[id].marks.push(markTimer);
+	}
+}
+
 TimeSet.prototype.end = function(name) {
-	let start = timer_id_for(name, "-start");
-	let end = process.hrtime(this.timers[start]);
+	let id = timer_id_for(name);
 
-	if (this.timers[start] != undefined) {
-		let id = timer_id_for(name, "-end");
-		this.timers[id] = end;
-		let elapsedTime = this.elapsedTimeFor(name);
-		let logName = name.charAt(0).toUpperCase() + name.slice(1) + ": ";
-		this.results.push(String(logName + elapsedTime));
+	if (this.timers[id] != undefined) {
+
+		if (this.timers[id].marks.length) {
+			let index = this.timers[id].marks.length;
+			this.timers[id].marks[index-1].end();
+		}
+
+		this.timers[id].timer.end();
 	}
 }
 
-/* Return string containing elapsed time for timer with 
- * name 'name', in order (seconds, milliseconds, nanoseconds)
- */
-TimeSet.prototype.elapsedTimeFor = function(name) {
-	let id = timer_id_for(name, "-end");
-	let timer = this.timers[id];
-	let ns = String(Number(timer[1]) + "ns");
-	let ms = String(Number(timer[1] / 1000000).toFixed(3) + "ms");
-	let sec = String(timer[0] + "s");
-	let formatted = "(" + sec + ", " + ms + ", " + ns + ")";
-	return formatted;
-}
+TimeSet.prototype.log = function(name) {
+	let id = timer_id_for(name);
 
-/* Return all timers and associated elapsed times */
-TimeSet.prototype.log = function() {
-	let result = "";
+	if (this.timers[id] != undefined) {
+		let logString = "";
 
-	for (let i=0; i<this.results.length; i++) {
-		result += String(this.results[i] + "\n");
+		let start = this.timers[id].timer.startTime;
+		let end = this.timers[id].timer.endTime;
+		let elapsed = end - start;
+		
+		logString += "---------------\n"
+		logString += name.charAt(0).toUpperCase() + name.slice(1) + ":\n\n"
+
+		let marks = this.timers[id].marks;
+		for (let i=0; i<marks.length; i++) {
+			let start = marks[i].startTime;
+			let end = marks[i].endTime;
+			let elapsed = end - start;
+			
+			logString += String(marks[i].name + ": " + format_time(elapsed) + "\n");
+		}
+		logString += "\n"
+		logString += String("Elapsed: " + format_time(elapsed) + "\n");
+		logString += "---------------\n"
+		console.log(logString);
 	}
-
-	return result;
 }
 
-function timer_id_for(name, type) {
-	return name.replace(/\s/g, '').toLowerCase().concat(type);
+function format_time(time) {
+	let ns = String(time + "ns");
+	let ms = String(Number(time / 1000000).toFixed(3) + "ms");
+	let s = String(Number(time / 1000000000).toFixed(3) + "s");
+	let formattedString = "(" + s + ", " + ms + ", " + ns + ")";
+	return formattedString;
 }
 
-module.exports = TimeSet;
+function timer_id_for(name) {
+	return name.replace(/\s/g, '').toLowerCase();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
